86b908920f61008b1566417bc53a0b7b598fed2d
igr <mixa_fc@ukr.net>
2018-02-07 18:38:56 +0200

    обновил последние изменения

--- src/test/java/resources/3.sql
+++ src/test/java/resources/3.sql
@@ -1,4 +1,6 @@
-SELECT SUM (developers.sallary) AS javaSalariesSUM
-FROM skills
-INNER JOIN developers ON developers.dev_id = skills.skill_id
-WHERE industry = 'Java';
+SELECT SUM(developers.sallary) AS javaSalariesSUM
+FROM developers, developer_skill
+WHERE developers.dev_id IN (
+  SELECT DISTINCT developer_skill.dev_id 
+  WHERE (developer_skill.skill_id BETWEEN 1 and 3)
+);
--- src/test/java/resources/6.sql
+++ src/test/java/resources/6.sql
@@ -1,6 +1,12 @@
-SELECT AVG(sallary)
-  FROM developers, project_developer, projects
-  WHERE developers.dev_id IN (
-    SELECT DISTINCT project_developer.dev_id
-    WHERE (project_developer.project_id = (SELECT projects.project_id WHERE (cost = (SELECT MIN (cost) FROM projects))))
-  );
+SELECT AVG(sallary) AS avarage_salaryAVG
+FROM developer_projects
+INNER JOIN (
+    SELECT project_id
+    FROM projects
+    ORDER BY cost ASC
+    LIMIT 1
+    ) projects ON projects.project_id = developer_projects.dev_id
+INNER JOIN (
+    SELECT dev_id, sallary
+    FROM developers
+    ) developers ON developers.dev_id = developer_projects.dev_id;
